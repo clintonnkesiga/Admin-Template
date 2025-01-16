c356edf3d8144f5abbc404061520ec7766a49a5d
nkesigaclinton <techninjaug@gmail.com>
2025-01-16 19:23:06 +0300

    Uploaded files

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Sidebar from "./components/Sidebar.vue";
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+})
+
+</script>
+
+<template>
+    <div id="app">
+      <Sidebar></Sidebar>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
